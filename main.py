9e644eab048317d59e9b413aef7c77067f872c96
nickole <nc427@njit.edu>
2024-02-29 14:15:51 -0500

    plugins added

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-# main.py
-from app import App    
+from app import App
 
+# Run the application
 if __name__ == "__main__":
-    app = App.start() #instantiates app instance
+    app = App()
+    app.load_plugins()
+
+    print("Type 'exit' to exit.")
+    while True:
+        user_input = input(">>> ").strip()
+        if user_input.lower() == 'exit':
+            break
+
+        app.command_handler.execute_command(user_input)
